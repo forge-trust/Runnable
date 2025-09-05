f11b1c59b0b16fa335588df6fabef50776058a85
Andrew Krock <andrew.krock@gmail.com>
2025-09-05 01:47:13 -0400

    feat(Aspire): Add initial support for Aspire

---
+++ Aspire/ForgeTrust.Runnable.Aspire/AspireApp.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection;
+using ForgeTrust.Runnable.Console;
+using ForgeTrust.Runnable.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ForgeTrust.Runnable.Aspire;
+
+public static class AspireApp
+{
+    public static Task RunAsync(string[] args)
+    {
+        var startupContext = new StartupContext(args, new AspireDefaultModule())
+        {
+            OverrideEntryPointAssembly = Assembly.GetCallingAssembly()
+        };
+
+        return new AspireAppStartup<AspireDefaultModule>().RunAsync(startupContext);
+    }
+}
+
+public static class AspireApp<TModule>
+    where TModule : IRunnableHostModule, new()
+{
+    public static Task RunAsync(string[] args) => new AspireAppStartup<TModule>()
+        .RunAsync(args);
+
+}
+
+internal class AspireAppStartup<TModule> : ConsoleStartup<TModule>
+    where TModule : IRunnableHostModule, new()
+{
+    protected override void ConfigureAdditionalServices(StartupContext context, IServiceCollection services)
+    {
+        var componentTypes = GetComponentTypes(context.EntryPointAssembly);
+
+        foreach(var type in componentTypes)
+        {
+            // We want to ensure that each component is registered as a singleton
+            // so that we don't have multiple instances of the same component.
+            services.AddSingleton(type);
+        }
+    }
+
+    private IReadOnlyList<Type> GetComponentTypes(Assembly hostAssembly)
+    {
+        var componentTypes = hostAssembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && typeof(IAspireComponent).IsAssignableFrom(t))
+            .ToList();
+
+        return componentTypes;
+    }
+}
+
+internal class AspireDefaultModule : IRunnableHostModule
+{
+
+    public void ConfigureHostAfterServices(StartupContext context, IHostBuilder builder)
+    {
+    }
+
+    public void ConfigureHostBeforeServices(StartupContext context, IHostBuilder builder)
+    {
+    }
+
+    public void ConfigureServices(StartupContext context, IServiceCollection services)
+    {
+
+    }
+
+    public void RegisterDependentModules(ModuleDependencyBuilder builder)
+    {
+    }
+}
---
+++ Aspire/ForgeTrust.Runnable.Aspire/AspireProfile.cs
@@ -0,0 +1,120 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using CliFx;
+using CliFx.Infrastructure;
+
+namespace ForgeTrust.Runnable.Aspire;
+
+public abstract class AspireProfile : ICommand
+{
+    // TODO: Need to implement this upstream: https://github.com/Tyrrrz/CliFx/issues/39
+    // It seems we need to be able to pass args upstream or this will cause
+    // problems trying to use deployments in Aspire.
+    public string[] PassThroughArgs => [];
+    public virtual IEnumerable<AspireProfile> GetDependencies()
+    {
+        return [];
+    }
+
+    public abstract IEnumerable<IAspireComponent> GetComponents();
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        var appBuilder = new DistributedApplicationBuilder(PassThroughArgs);
+        var context = new AspireStartupContext(appBuilder);
+
+        foreach (var profile in GetDependencies())
+        {
+            foreach (var component in profile.GetComponents())
+            {
+                if (component is IAspireComponent<IResource> typedComponent)
+                {
+                    context.Resolve(typedComponent);
+                }
+            }
+        }
+
+        foreach (var component in GetComponents())
+        {
+            if (component is IAspireComponent<IResource> typedComponent)
+            {
+                context.Resolve(typedComponent);
+            }
+        }
+
+        var app = appBuilder.Build();
+        await app.RunAsync();
+    }
+}
+
+public interface IAspireComponent<out T> : IAspireComponent
+    where T : IResource
+{
+
+    IResourceBuilder<T> Generate(
+        AspireStartupContext context,
+        IDistributedApplicationBuilder appBuilder);
+}
+
+public interface IAspireComponent
+{
+
+}
+
+public class AspireStartupContext
+{
+    private readonly IDistributedApplicationBuilder _appBuilder;
+    private readonly Dictionary<IAspireComponent, object> _resolvedComponents = new();
+    private readonly string _applicationRoot;
+
+    public AspireStartupContext(
+        IDistributedApplicationBuilder appBuilder,
+        string? applicationRoot = null)
+    {
+        _appBuilder = appBuilder;
+        _applicationRoot = applicationRoot ?? FindRepoRoot();
+    }
+
+    public IResourceBuilder<TResource> Resolve<TResource>(IAspireComponent<TResource> dependency)
+        where TResource : IResource
+    {
+        if (!_resolvedComponents.TryGetValue(dependency, out var rssBuilder))
+        {
+            rssBuilder = dependency.Generate(this, _appBuilder);
+            _resolvedComponents[dependency] = rssBuilder;
+        }
+
+        return (IResourceBuilder<TResource>)rssBuilder;
+    }
+
+    public string GetPathFromRoot(string relativePath)
+    {
+        return Path.Combine(_applicationRoot, relativePath);
+    }
+
+    private static string FindRepoRoot()
+    {
+        DirectoryInfo? dir = new DirectoryInfo(AppContext.BaseDirectory);
+        var lastRepo = default(string);
+        while (dir != null)
+        {
+            // TODO: handle other repo types.
+            var isRepo = Directory.Exists(Path.Combine(dir.FullName, ".git"));
+            if (isRepo)
+            {
+                lastRepo = dir.FullName;
+            }
+
+            dir = dir.Parent;
+        }
+
+        if (lastRepo != null)
+        {
+            return lastRepo;
+        }
+
+        // TODO: Is there a better default we can use here?
+        return string.Empty;
+    }
+}
+

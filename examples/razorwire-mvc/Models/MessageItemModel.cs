36ff47c189d60a39f4d8fba72335a15f289597a8
Andrew Krock <andrew.krock@gmail.com>
2026-02-06 20:00:02 -0800

    docs: add xml documentation to MessageItemModel

--- examples/razorwire-mvc/Models/MessageItemModel.cs
+++ examples/razorwire-mvc/Models/MessageItemModel.cs
@@ -1,3 +1,9 @@
 namespace RazorWireWebExample.Models;
 
+/// <summary>
+/// Represents a chat message item to be rendered in the UI.
+/// </summary>
+/// <param name="DisplayName">The display name of the user who sent the message.</param>
+/// <param name="UtcTime">The UTC timestamp of the message, formatted as a string.</param>
+/// <param name="Message">The content of the message.</param>
 public record MessageItemModel(string DisplayName, string UtcTime, string Message);
